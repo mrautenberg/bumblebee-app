3f76460cdfdb9595c84709b3f0596ad845712312
Max Rautenberg <max.rautenberg89@gmail.com>
2021-08-08 17:44:56 +0200

    signup

--- src/components/Firebase/context.js
+++ src/components/Firebase/context.js
@@ -3,4 +3,11 @@ import React from "react";
 // Create provider and consumer
 const FirebaseContext = React.createContext(null);
 
+export const withFirebase = (Component) => (props) =>
+  (
+    <FirebaseContext.Consumer>
+      {(firebase) => <Component {...props} firebase={firebase} />}
+    </FirebaseContext.Consumer>
+  );
+
 export default FirebaseContext;
