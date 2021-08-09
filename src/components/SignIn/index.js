241fcb860562b9136cb15c4529bc9874a83fc98c
Max Rautenberg <max.rautenberg89@gmail.com>
2021-08-09 09:46:34 +0200

    sign in functionality

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,7 +1,84 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
-const SignIn = () => {
-  return <div>Sign In</div>;
+import { SignUpLink } from "../SignUp";
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
+
+const SignInPage = () => (
+  <div>
+    <h1>Sign In</h1>
+    <SignInForm />
+    <SignUpLink />
+  </div>
+);
+
+const INITIAL_STATE = {
+  email: "",
+  password: "",
+  error: null,
 };
 
-export default SignIn;
+class SignInFormBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = (event) => {
+    const { email, password } = this.state;
+
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch((error) => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  };
+
+  onChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { email, password, error } = this.state;
+
+    const isInvalid = password === "" || email === "";
+
+    return (
+      <form>
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign In
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+const SignInForm = withRouter(withFirebase(SignInFormBase));
+
+export default SignInPage;
+
+export { SignInForm };
