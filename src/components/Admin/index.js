a27de919e2a1c97059ab0d973d69f800b3a68a52
Max Rautenberg <max.rautenberg89@gmail.com>
2021-08-10 13:35:06 +0200

    authorization by roles

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { compose } from "recompose";
 
 import { withFirebase } from "../Firebase";
+import { withAuthorization } from "../Session";
+import * as ROLES from "../../constants/roles";
 
 class AdminPage extends Component {
   constructor(props) {
@@ -37,6 +40,7 @@ class AdminPage extends Component {
     return (
       <div>
         <h1>Admin</h1>
+        <p>The Admin Page is accesible by every signed in admin user.</p>
 
         {loading && <div>Loading...</div>}
         <UserList users={users} />
@@ -63,4 +67,6 @@ const UserList = ({ users }) => (
   </ul>
 );
 
-export default withFirebase(AdminPage);
+const condition = (authUser) => authUser && !!authUser.roles[ROLES.ADMIN];
+
+export default compose(withAuthorization(condition), withFirebase)(AdminPage);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,18 +3,25 @@ import { Link } from "react-router-dom";
 
 import SignOutButton from "../SignOut";
 import * as ROUTES from "../../constants/routes";
+import * as ROLES from "../../constants/roles";
 
 import { AuthUserContext } from "../Session";
 
 const Navigation = () => (
   <div>
     <AuthUserContext.Consumer>
-      {(authUser) => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
+      {(authUser) =>
+        authUser ? (
+          <NavigationAuth authUser={authUser} />
+        ) : (
+          <NavigationNonAuth />
+        )
+      }
     </AuthUserContext.Consumer>
   </div>
 );
 
-const NavigationAuth = () => (
+const NavigationAuth = ({ authUser }) => (
   <ul>
     <li>
       <Link to={ROUTES.LANDING}>Landing</Link>
@@ -25,9 +32,12 @@ const NavigationAuth = () => (
     <li>
       <Link to={ROUTES.ACCOUNT}>Account</Link>
     </li>
-    <li>
-      <Link to={ROUTES.ADMIN}>Admin</Link>
-    </li>
+    {!!authUser.roles[ROLES.ADMIN] && (
+      <li>
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+    )}
+
     <li>
       <SignOutButton />
     </li>
